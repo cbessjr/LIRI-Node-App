3e71ca377cea385efab5e99013c7bc71deef585f
Ender <cbessjr@gmail.com>
2018-12-16 15:50:11 -0700

    0300 121618 Uninstall spotify and installed Node-Spotify-API.

--- liri.js
+++ liri.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 
 //Spotoify Keys
 var keys = require("./keys.js");
+var spotify = require('node-spotify-api');
+
 console.log(keys);
-// var Spotify = new Spotify(keys.spotify);
+var spotify = new spotify(keys.spotify);
 
 
-// var spotify = require('spotify');
 
 // // var song = process.argv.slice(3).join(" ");
 
@@ -15,7 +16,7 @@ console.log(keys);
 // var result = spotify.search: function({ type: 'artist OR album OR track', query: 'My search query' }, hollaback)
 
 
-var spotify = require('spotify');
+// var spotify = require('spotify');
 
 spotify.search({ type: 'track', query: 'dancing in the moonlight', id: '2cdd6a42f4f4669aab9337cc3a6ed06' }, function(err, data) {
     if ( err ) {
@@ -26,3 +27,57 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight', id: '2cdd6a42
     console.log(data);
 
 });
+
+
+
+
+
+
+switch (action) {
+    case "concert-this":
+        concert();
+        break;
+
+    case "spotify-this-song":
+        spotify();
+        break;
+
+    case "movie-this":
+        movie();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default:
+
+    console.log("Placeholder Text");
+
+}
+
+
+//Function(s) Code
+function concert() {
+
+
+
+};
+
+function spotify() {
+
+
+
+};
+
+function movie() {
+
+
+
+};
+
+function doWhatItSays() {
+
+
+
+};
