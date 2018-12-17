27b63fd139a4e79741e208b13f7d9a846470c124
Ender <cbessjr@gmail.com>
2018-12-16 17:31:00 -0700

    0530 121618 Working on the spotify communication.

--- liri.js
+++ liri.js
@@ -12,12 +12,8 @@ var Spotify = require('node-spotify-api');
 
 // var song = process.argv.slice(2).join(" ");
 
-var Spotify = require('node-spotify-api');
  
-var spotify = new Spotify({
-  id: e2cdd6a42f4f4669aab9337cc3a6ed06,
-  secret: 35f5dd7986c1480aa4ad72c5bba40d03
-});
+var spotify = new Spotify(keys.spotify);
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
