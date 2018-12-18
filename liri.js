f45fa114e6866ff2562043150d613e0c00cff56d
Ender <cbessjr@gmail.com>
2018-12-18 08:15:07 -0700

    0814 121818 Working on the  BandsInTown API.

--- liri.js
+++ liri.js
@@ -6,6 +6,9 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+var APP_ID = "codingbootcamp";
+var Bandsintown = require('bandsintown')(APP_ID);
+var bandsintown = new Bandsintown(APP_ID);
 
 var action = process.argv[2];
 
@@ -39,6 +42,40 @@ switch (action) {
 //Function(s) Code
 function concert() {
 
+    var band = process.argv.slice(3). join(" ");
+
+    if (band === "") {
+
+        console.log("Please enter a concert search with the syntax: node liri.js concert-this <artist/band name here>");
+
+    } else {
+
+        axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
+
+ 
+        // bandsintown
+        // .getArtistEventList('Skrillex')
+        // .then(function(events) {
+        //     // return array of events
+
+        //     console.log(data);
+
+            // var eventsArray = [];
+
+            // for (var i = 0; i < data.tracks.items.length; i++) {
+            //     var result = {
+            //         artist: data.tracks.items[i].album.artists[0].name,
+            //         album_name: data.tracks.items[i].album.name,
+            //         song_name: data.tracks.items[i].name,
+            //         preview_url: data.tracks.items[i].preview_url
+            //     }
+            //     eventsArray.push(result);
+            // }
+
+//   });
+
+
+    }
 
 };
 
